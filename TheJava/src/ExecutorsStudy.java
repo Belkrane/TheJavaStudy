de93742b24f6cb2828b41ea5f6b5576666eee2b6
Belkrane <jonghagi77@gmail.com>
2023-07-24 12:37:24 +0900

    Callable and Future

--- TheJava/src/ExecutorsStudy.java
+++ TheJava/src/ExecutorsStudy.java
@@ -1,13 +1,12 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class ExecutorsStudy {
 
     //Executor가 Thread를 만들고, 우리는 runnable만 제공해주면 된다.
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
 
         executorService.execute(new Runnable() {
@@ -17,7 +16,7 @@ public class ExecutorsStudy {
             }
         });
 
-        ExecutorService executorService2 = Executors.newSingleThreadExecutor();
+        ExecutorService executorService2 = Executors.newFixedThreadPool(4);
         executorService2.submit(() -> System.out.println("Thread2 " + Thread.currentThread().getName()));
 
         //ExecutorService는 실행이 되고나면, 다음 호출까지 기다리기 때문에 shutdown을 해줘야함.
@@ -32,10 +31,56 @@ public class ExecutorsStudy {
         executorService2.submit(getRunnable("4"));
         executorService2.submit(getRunnable("5"));
 
-        executorService2.shutdown();
+        //executorService2.shutdown();
 
+        //ScheduledExecutorService는 Executor인데 스케쥴 기능을 포함하고있다. 딜레이를 줘서 몇 초 후에 실행이 되거나
+        //특정 간격으로 반복 시킬 수 있음
         ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
         scheduledExecutorService.scheduleAtFixedRate(getRunnable("Hello"), 1, 2, TimeUnit.SECONDS);
+        scheduledExecutorService.shutdown();
+        //Callable => Runnable과 같은데 Return값을 가질 수 있다.
+
+        Callable<String> hello = () -> {
+            Thread.sleep(2000L);
+            return "Hello";
+        };
+
+        Callable<String> java = () -> {
+            Thread.sleep(3000L);
+            return "java";
+        };
+
+        Callable<String> belk = () -> {
+            Thread.sleep(1000L);
+            return "belk";
+        };
+
+
+        //invokeAll 을 이용해서 한 번에 여러개의 Callable을 줄수 있다.
+        //-> 여러개의 쓰레드가 모두 끝날때 까지 기다렸다가 한꺼번에 return함
+        System.out.println("invokeAll ==============================");
+        List<Future<String>> futureList =  executorService2.invokeAll(Arrays.asList(hello, java, belk));
+        for(Future<String> f : futureList){
+            System.out.println(f.get());
+        }
+
+        System.out.println("invokeAny ==============================");
+        //모두 기다리지 않고 하나만 응답이 와도 끝내고 싶은 경우에는 invokeAny를 사용하면 된다
+        String s = executorService2.invokeAny(Arrays.asList(hello, java, belk));
+        System.out.println(s);
+
+
+        Future<String> submit = executorService2.submit(hello);
+        System.out.println(submit.isDone());
+        System.out.println("Started!");
+
+        System.out.println(submit.get()); // 블록킹
+
+        System.out.println("End!!");
+
+
+        executorService2.shutdown();
+
     }
 
 
